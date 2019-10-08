03bf4c0680ccae3e07915b5b0877a7b447f0dc69
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-10-08 17:49:00 +0200

    File Selector added

---
+++ components/FileSelector.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="file" accept="image/*" @change="handleFileChange" />
+</template>
+
+<script>
+export default {
+  methods: {
+    handleFileChange(e) {
+      const reader = new FileReader()
+      reader.onload = (ev) => this.$emit('input', ev.target.result)
+      reader.readAsDataURL(e.target.files[0])
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,51 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         pixelsorterino
       </h1>
-      <h2 class="subtitle">
-        sort the pixels
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+
+      <img id="SourceImage" :src="file" alt="source" />
+      <canvas id="Canvas"></canvas>
+
+      <FileSelector v-model="file"></FileSelector>
+      <button @click="sort">Sort</button>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import FileSelector from '~/components/FileSelector'
 
 export default {
   components: {
-    Logo
+    FileSelector
+  },
+  data() {
+    return {
+      file: null
+    }
+  },
+  methods: {
+    sort() {
+      // eslint-disable-next-line
+      console.log(this.file)
+
+      const image = document.getElementById('SourceImage')
+      const canvas = document.getElementById('Canvas')
+      const context = canvas.getContext('2d')
+
+      canvas.width = image.width
+      canvas.height = image.height
+
+      context.drawImage(image, 0, 0)
+
+      const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+
+      // sort image here
+
+      context.putImageData(imageData, 0, 0)
+    }
   }
 }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
