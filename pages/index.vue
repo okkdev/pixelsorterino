ea192bc5516121c6d92350b7e29bfbb086ac7368
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-10-15 17:04:44 +0200

    added hsv sorting

--- pages/index.vue
+++ pages/index.vue
@@ -40,32 +40,58 @@ export default {
 
       context.drawImage(image, 0, 0)
 
-      const imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+      const imageData = context.getImageData(0, 0, image.width, image.height)
 
-      let pixelarray = Array.from(imageData.data)
+      const pixelArray = this.spliceArray(
+        Array.from(imageData.data).flat(Infinity),
+        4
+      )
 
-      pixelarray = this.spliceArray(pixelarray.flat(Infinity), 4)
+      // const sortedPixelArray = pixelArray
+      //   .map((c, i) => ({ color: this.rgbToHsl(c), index: i }))
+      //   .sort((a, b) => b.color[0] - a.color[0])
+      //   .map((data) => pixelArray[data.index])
 
-      pixelarray.sort(function(a, b) {
-        return a[1] > b[1] ? 1 : -1
-      })
+      const sortedPixelArray = pixelArray.sort((a, b) => a[0] > b[0])
 
-      // eslint-disable-next-line
-      console.log(pixelarray)
-
-      const modImage = new ImageData(
-        new Uint8ClampedArray(pixelarray.flat(Infinity)),
+      const sortedImage = new ImageData(
+        new Uint8ClampedArray(sortedPixelArray.flat(Infinity)),
         imageData.width,
         imageData.height
       )
 
-      context.putImageData(modImage, 0, 0)
+      context.putImageData(sortedImage, 0, 0)
     },
     spliceArray(arr, size) {
       const res = []
       for (let i = 0; i < arr.length; i = i + size)
         res.push(arr.slice(i, i + size))
       return res
+    },
+    rgbToHsl(rgb) {
+      const r = rgb[0] / 255
+      const g = rgb[1] / 255
+      const b = rgb[2] / 255
+
+      let h = 0
+      let s = 0
+      let v = 0
+
+      const minRGB = Math.min(r, Math.min(g, b))
+      const maxRGB = Math.max(r, Math.max(g, b))
+
+      if (minRGB === maxRGB) {
+        v = minRGB
+        return [0, 0, v]
+      }
+
+      const x = r === minRGB ? g - b : b === minRGB ? r - g : b - r
+      const y = r === minRGB ? 3 : b === minRGB ? 1 : 5
+      h = 60 * (y - x / (maxRGB - minRGB))
+      s = (maxRGB - minRGB) / maxRGB
+      v = maxRGB
+
+      return [h, s, v]
     }
   }
 }
