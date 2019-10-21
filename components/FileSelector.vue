49d0bc7f44ebde3cc43a39742686d7d071d9ba79
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-10-21 15:59:32 +0200

    drag and drop support

--- components/FileSelector.vue
+++ components/FileSelector.vue
@@ -1,5 +1,13 @@
 <template>
-  <input type="file" accept="image/*" @change="handleFileChange" />
+  <div>
+    <input
+      type="file"
+      class="border-4 border-dashed border-teal-500 rounded-lg p-4 my-3 w-full text-center"
+      accept="image/jpeg, image/png, image/gif"
+      @change="handleFileChange"
+      @drop="dropFile"
+    />
+  </div>
 </template>
 
 <script>
@@ -9,6 +17,11 @@ export default {
       const reader = new FileReader()
       reader.onload = (ev) => this.$emit('input', ev.target.result)
       reader.readAsDataURL(e.target.files[0])
+    },
+    dropFile(e) {
+      const reader = new FileReader()
+      reader.onload = (ev) => this.$emit('input', ev.target.result)
+      reader.readAsDataURL(e.dataTransfer.files[0])
     }
   }
 }
