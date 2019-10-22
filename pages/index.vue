a9eebfc0dc71d3034ca1dfbac683fd4b93321c98
okkdev <stehlik.jen@gmail.com>
2019-10-22 13:24:42 +0200

    hotfix upload

--- pages/index.vue
+++ pages/index.vue
@@ -148,6 +148,8 @@ export default {
             this.sortedImageSmall = cvs.toDataURL()
             resolve()
           }
+        } else {
+          resolve('image too small')
         }
       })
     },
